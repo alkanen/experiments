63ce33db5f9a39e5492f4d27a1d57d9afe7775b7
Daniel Lindsäth <alkanen@gmail.com>
2021-02-16 19:08:35 +0000

    [raytracer tutorial] Step 7 done.

--- ray-tracer/tutorial_in_a_weekend/rtweekend.hpp
+++ ray-tracer/tutorial_in_a_weekend/rtweekend.hpp
@@ -28,4 +28,22 @@ inline double degrees_to_radians(double degrees) {
 #include "ray.hpp"
 #include "vec3.hpp"
 
+inline double random_double() {
+  // Returns a random real in [0,1).
+  return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+  // Returns a random real in [min,max).
+  return min + (max-min)*random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+  if (x < min)
+    return min;
+  if (x > max)
+    return max;
+  return x;
+}
+
 #endif
