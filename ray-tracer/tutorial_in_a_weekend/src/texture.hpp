3555f9d1c77b9e85677713e25673fd3f5c635768
Daniel Lindsäth <alkanen@gmail.com>
2021-03-03 17:16:20 +0000

    [raytracer tutorial pt2] Section 4, solid textures and scene selector.

---
+++ ray-tracer/tutorial_in_a_weekend/src/texture.hpp
@@ -0,0 +1,50 @@
+#ifndef TEXTURE_HPP
+#define TEXTURE_HPP
+
+#include "rtweekend.hpp"
+
+class Texture {
+public:
+  virtual Color value(double u, double v, const Point3 &p) const = 0;
+};
+
+class SolidColor : public Texture {
+public:
+  SolidColor() {}
+  SolidColor(Color c) : color_value(c) {}
+
+  SolidColor(double red, double green, double blue)
+    : SolidColor(Color(red, green, blue)) {}
+
+  virtual Color value(double u, double v, const Vec3 &p) const override {
+    return color_value;
+  }
+
+private:
+  Color color_value;
+};
+
+class CheckerTexture : public Texture {
+public:
+  CheckerTexture() {}
+
+  CheckerTexture(Color c1, Color c2)
+    : even(new SolidColor(c1)), odd(new SolidColor(c2)) {}
+  CheckerTexture(Texture *even, Texture *odd)
+    : even(even), odd(odd) {}
+
+  virtual Color value(double u, double v, const Point3 &p) const override
+  {
+    auto sines = sin(10*p.x())*sin(10*p.y())*sin(10*p.z());
+    if (sines < 0)
+      return odd->value(u, v, p);
+    else
+      return even->value(u, v, p);
+  }
+
+public:
+  Texture *even;
+  Texture *odd;
+};
+
+#endif
