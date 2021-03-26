9236554f29e899cdf30aa2a8d510d59ad8e2aed4
Daniel Lindsäth <alkanen@gmail.com>
2021-03-26 21:44:59 +0000

    [raytracer refactoring] Bvh, ConstantMedium and MovingSphere split into header and implementation.

---
+++ ray-tracer/tutorial_in_a_weekend/src/constant_medium.cpp
@@ -0,0 +1,73 @@
+#include "constant_medium.hpp"
+
+#include "hittable.hpp"
+#include "ray.hpp"
+#include "aabb.hpp"
+
+ConstantMedium::ConstantMedium(Hittable *b, double d, Texture *a)
+  : boundary(b)
+  , neg_inv_density(-1/d)
+  , phase_function(new Isotropic(a))
+{
+}
+
+ConstantMedium::ConstantMedium(Hittable *b, double d, Color c)
+  : boundary(b)
+  , neg_inv_density(-1/d)
+  , phase_function(new Isotropic(c))
+{
+}
+
+bool ConstantMedium::hit(const Ray &ray, double t_min, double t_max, HitRecord &rec) const
+{
+  // Print occasional samples when debugging. To enable, set enableDebug true.
+  const bool enableDebug = false;
+  const bool debugging = enableDebug && random_double() < 0.00001;
+
+  HitRecord rec1, rec2;
+
+  if(!boundary->hit(ray, -infinity, infinity, rec1))
+    return false;
+
+  if(!boundary->hit(ray, rec1.t+0.0001, infinity, rec2))
+    return false;
+
+  if(debugging) std::cerr << "\nt_min=" << rec1.t << ", t_max=" << rec2.t << '\n';
+
+  if(rec1.t < t_min) rec1.t = t_min;
+  if(rec2.t > t_max) rec2.t = t_max;
+
+  if(rec1.t >= rec2.t)
+    return false;
+
+  if(rec1.t < 0)
+    rec1.t = 0;
+
+  const auto ray_length = ray.direction().length();
+  const auto distance_inside_boundary = (rec2.t - rec1.t) * ray_length;
+  const auto hit_distance = neg_inv_density * log(random_double());
+
+  if(hit_distance > distance_inside_boundary)
+    return false;
+
+  rec.t = rec1.t + hit_distance / ray_length;
+  rec.p = ray.at(rec.t);
+
+  if(debugging) {
+    std::cerr << "hit_distance = " <<  hit_distance << '\n'
+              << "rec.t = " <<  rec.t << '\n'
+              << "rec.p = " <<  rec.p << '\n';
+  }
+
+  rec.normal = Vec3(1, 0, 0);  // arbitrary
+  rec.front_face = true;       // also arbitrary
+  rec.material = phase_function;
+
+  return true;
+}
+
+bool ConstantMedium::bounding_box(double time0, double time1, Aabb& output_box) const
+{
+  return boundary->bounding_box(time0, time1, output_box);
+}
+
--- ray-tracer/tutorial_in_a_weekend/src/moving_sphere.hpp
+++ ray-tracer/tutorial_in_a_weekend/src/moving_sphere.hpp
@@ -1,15 +1,15 @@
 #ifndef MOVING_SPHERE_HPP
 #define MOVING_SPHERE_HPP
 
-#include "rtweekend.hpp"
-
 #include "hittable.hpp"
-#include "aabb.hpp"
 #include "material.hpp"
+#include "vec3.hpp"
+#include "ray.hpp"
+#include "aabb.hpp"
 
 class MovingSphere : public Hittable {
 public:
-  MovingSphere() {}
+  MovingSphere();
   MovingSphere(
     Point3 center0,
     Point3 center1,
@@ -17,33 +17,7 @@ public:
     double time1,
     double radius,
     Material *material
-  ) :
-    center0(center0),
-    center1(center1),
-    time0(time0),
-    time1(time1),
-    radius(radius),
-    material(material)
-  {
-    /*
-    std::cerr
-      << "MovingSphere("
-      << center0
-      << ", "
-      << center1
-      << ", "
-      << time0
-      << ", "
-      << time1
-      << ", "
-      << radius
-      << ", "
-      << *material
-      << ")"
-      << std::endl;
-    */
-  }
-
+  );
   virtual bool hit(
     const Ray &ray, double t_min, double t_max, HitRecord &rec
   ) const override;
@@ -60,53 +34,5 @@ public:
   double radius;
   Material *material;
 };
-
-Point3 MovingSphere::center(double time) const
-{
-  return center0 + ((time - time0) / (time1 - time0)) * (center1 - center0);
-}
-
-bool MovingSphere::hit(const Ray& ray, double t_min, double t_max, HitRecord& rec) const
-{
-  Vec3 oc = ray.origin() - center(ray.time());
-  auto a = ray.direction().length_squared();
-  auto half_b = dot(oc, ray.direction());
-  auto c = oc.length_squared() - radius * radius;
-
-  auto discriminant = half_b * half_b - a * c;
-  if(discriminant < 0)
-    return false;
-  auto sqrtd = sqrt(discriminant);
-
-  // Find the nearest root that lies in the acceptable range.
-  auto root = (-half_b - sqrtd) / a;
-  if (root < t_min || t_max < root) {
-    root = (-half_b + sqrtd) / a;
-    if (root < t_min || t_max < root)
-      return false;
-  }
-
-  rec.t = root;
-  rec.p = ray.at(rec.t);
-  auto outward_normal = (rec.p - center(ray.time())) / radius;
-  rec.set_face_normal(ray, outward_normal);
-  rec.material = material;
-
-  return true;
-}
-
-bool MovingSphere::bounding_box(double time0, double time1, Aabb &output_box) const
-{
-  Aabb box0(
-    center(time0) - Vec3(radius, radius, radius),
-    center(time1) + Vec3(radius, radius, radius)
-  );
-  Aabb box1(
-    center(time1) - Vec3(radius, radius, radius),
-    center(time1) + Vec3(radius, radius, radius)
-  );
-  output_box = surrounding_box(box0, box1);
-  return true;
-}
 
 #endif
