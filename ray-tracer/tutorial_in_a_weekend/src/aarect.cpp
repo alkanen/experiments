bcfc441fd203ec99d336870a37ccefae7b1d33be
Daniel Lindsäth <alkanen@gmail.com>
2021-03-26 13:34:04 +0000

    [raytracer refactoring] Aarect split into header and implementation.

---
+++ ray-tracer/tutorial_in_a_weekend/src/aarect.cpp
@@ -0,0 +1,164 @@
+#include "aarect.hpp"
+
+#include "hittable.hpp"
+#include "material.hpp"
+#include "ray.hpp"
+
+XyRect::XyRect()
+{
+}
+
+XyRect::XyRect(
+  double x0, double x1, double y0, double y1, double k, Material *mat
+)
+  : x0(x0)
+  , x1(x1)
+  , y0(y0)
+  , y1(y1)
+  , k(k)
+  , material(mat)
+{
+}
+
+bool XyRect::bounding_box(double time0, double time1, Aabb &output_box) const
+{
+  // The bounding box must have non-zero width in each dimension, so pad the Z
+  // dimension a small amount.
+  output_box = Aabb(Point3(x0, y0, k-0.0001), Point3(x1, y1, k+0.0001));
+  return true;
+}
+
+bool XyRect::hit(const Ray &r, double t_min, double t_max, HitRecord &rec) const
+{
+  auto t = (k-r.origin().z()) / r.direction().z();
+  if(t < t_min || t > t_max)
+    return false;
+
+  auto x = r.origin().x() + t*r.direction().x();
+  auto y = r.origin().y() + t*r.direction().y();
+  if(x < x0 || x > x1 || y < y0 || y > y1)
+    return false;
+
+  rec.u = (x - x0) / (x1 - x0);
+  rec.v = (y - y0) / (y1 - y0);
+  rec.t = t;
+
+  auto outward_normal = Vec3(0, 0, 1);
+  rec.set_face_normal(r, outward_normal);
+  rec.material = material;
+  rec.p = r.at(t);
+
+  return true;
+}
+
+XzRect::XzRect()
+{
+}
+
+XzRect::XzRect(double x0, double x1, double z0, double z1, double k, Material *mat)
+  : x0(x0)
+  , x1(x1)
+  , z0(z0)
+  , z1(z1)
+  , k(k)
+  , material(mat)
+{
+}
+
+bool XzRect::bounding_box(double time0, double time1, Aabb &output_box) const
+{
+  // The bounding box must have non-zero width in each dimension, so pad the Z
+  // dimension a small amount.
+  output_box = Aabb(Point3(x0, k-0.0001, z0), Point3(x1, k+0.0001, z1));
+  return true;
+}
+
+double XzRect::pdf_value(const Point3 &o, const Vec3 &v) const
+{
+  HitRecord rec;
+  if(!this->hit(Ray(o, v), 0.001, infinity, rec)) {
+    // std::cout << "XzRect::pdf_value(" << o << ", " << v << ") -> 0 (not hit)" << std::endl;
+    return 0;
+  }
+
+  auto area = (x1 - x0) * (z1 - z0);
+  auto distance_squared = rec.t * rec.t * v.length_squared();
+  auto cosine = fabs(dot(v, rec.normal) / v.length());
+  auto retval = distance_squared / (cosine * area);
+  // std::cout << "XzRect::pdf_value(" << o << ", " << v << ") -> " << retval << std::endl;
+  return retval;
+}
+
+Vec3 XzRect::random(const Vec3 &o) const
+{
+  auto random_point = Point3(random_double(x0, x1), k, random_double(z0, z1));
+  return random_point - o;
+}
+
+bool XzRect::hit(const Ray &r, double t_min, double t_max, HitRecord &rec) const
+{
+  auto t = (k-r.origin().y()) / r.direction().y();
+  if(t < t_min || t > t_max)
+    return false;
+
+  auto x = r.origin().x() + t*r.direction().x();
+  auto z = r.origin().z() + t*r.direction().z();
+  if(x < x0 || x > x1 || z < z0 || z > z1)
+    return false;
+
+  rec.u = (x - x0) / (x1 - x0);
+  rec.v = (z - z0) / (z1 - z0);
+  rec.t = t;
+
+  auto outward_normal = Vec3(0, 1, 0);
+  rec.set_face_normal(r, outward_normal);
+  rec.material = material;
+  rec.p = r.at(t);
+
+  return true;
+}
+
+YzRect::YzRect()
+{
+}
+
+YzRect::YzRect(double y0, double y1, double z0, double z1, double k, Material *mat)
+  : y0(y0)
+  , y1(y1)
+  , z0(z0)
+  , z1(z1)
+  , k(k)
+  , material(mat)
+{
+}
+
+bool YzRect::bounding_box(double time0, double time1, Aabb &output_box) const
+{
+  // The bounding box must have non-zero width in each dimension, so pad the Z
+  // dimension a small amount.
+  output_box = Aabb(Point3(k-0.0001, y0, z0), Point3(k+0.0001, y1, z1));
+  return true;
+}
+
+bool YzRect::hit(const Ray &r, double t_min, double t_max, HitRecord &rec) const
+{
+  auto t = (k-r.origin().x()) / r.direction().x();
+  if(t < t_min || t > t_max)
+    return false;
+
+  auto y = r.origin().y() + t*r.direction().y();
+  auto z = r.origin().z() + t*r.direction().z();
+  if(y < y0 || y > y1 || z < z0 || z > z1)
+    return false;
+
+  rec.u = (y - y0) / (y1 - y0);
+  rec.v = (z - z0) / (z1 - z0);
+  rec.t = t;
+
+  auto outward_normal = Vec3(1, 0, 0);
+  rec.set_face_normal(r, outward_normal);
+  rec.material = material;
+  rec.p = r.at(t);
+
+  return true;
+}
--- ray-tracer/tutorial_in_a_weekend/src/aarect.hpp
+++ ray-tracer/tutorial_in_a_weekend/src/aarect.hpp
@@ -3,169 +3,55 @@
 
 #include "vec3.hpp"
 #include "ray.hpp"
-
 #include "hittable.hpp"
 
 class XyRect : public Hittable {
 public:
-  XyRect() {}
-
-  XyRect(
-    double x0, double x1, double y0, double y1, double k, Material *mat
-  )
-    : x0(x0), x1(x1), y0(y0), y1(y1), k(k), material(mat) {};
+  XyRect();
+  XyRect(double x0, double x1, double y0, double y1, double k, Material *mat);
 
   virtual bool hit(const Ray &r, double t_min, double t_max, HitRecord &rec) const override;
 
-  virtual bool bounding_box(double time0, double time1, Aabb &output_box) const override
-  {
-    // The bounding box must have non-zero width in each dimension, so pad the Z
-    // dimension a small amount.
-    output_box = Aabb(Point3(x0, y0, k-0.0001), Point3(x1, y1, k+0.0001));
-    return true;
-  }
+  virtual bool bounding_box(double time0, double time1, Aabb &output_box) const override;
 
 public:
   double x0, x1, y0, y1, k;
   Material *material;
 };
 
-bool XyRect::hit(const Ray &r, double t_min, double t_max, HitRecord &rec) const
-{
-  auto t = (k-r.origin().z()) / r.direction().z();
-  if(t < t_min || t > t_max)
-    return false;
-
-  auto x = r.origin().x() + t*r.direction().x();
-  auto y = r.origin().y() + t*r.direction().y();
-  if(x < x0 || x > x1 || y < y0 || y > y1)
-    return false;
-
-  rec.u = (x - x0) / (x1 - x0);
-  rec.v = (y - y0) / (y1 - y0);
-  rec.t = t;
-
-  auto outward_normal = Vec3(0, 0, 1);
-  rec.set_face_normal(r, outward_normal);
-  rec.material = material;
-  rec.p = r.at(t);
-
-  return true;
-}
 
 class XzRect : public Hittable {
 public:
-  XzRect() {}
-
-  XzRect(
-    double x0, double x1, double z0, double z1, double k, Material *mat
-  )
-    : x0(x0), x1(x1), z0(z0), z1(z1), k(k), material(mat) {};
+  XzRect();
+  XzRect(double x0, double x1, double z0, double z1, double k, Material *mat);
 
   virtual bool hit(const Ray &r, double t_min, double t_max, HitRecord &rec) const override;
 
-  virtual bool bounding_box(double time0, double time1, Aabb &output_box) const override
-  {
-    // The bounding box must have non-zero width in each dimension, so pad the Z
-    // dimension a small amount.
-    output_box = Aabb(Point3(x0, k-0.0001, z0), Point3(x1, k+0.0001, z1));
-    return true;
-  }
+  virtual bool bounding_box(double time0, double time1, Aabb &output_box) const override;
 
-  virtual double pdf_value(const Point3 &o, const Vec3 &v) const
-  {
-    HitRecord rec;
-    if(!this->hit(Ray(o, v), 0.001, infinity, rec)) {
-      // std::cout << "XzRect::pdf_value(" << o << ", " << v << ") -> 0 (not hit)" << std::endl;
-      return 0;
-    }
+  virtual double pdf_value(const Point3 &o, const Vec3 &v) const;
 
-    auto area = (x1 - x0) * (z1 - z0);
-    auto distance_squared = rec.t * rec.t * v.length_squared();
-    auto cosine = fabs(dot(v, rec.normal) / v.length());
-    auto retval = distance_squared / (cosine * area);
-    // std::cout << "XzRect::pdf_value(" << o << ", " << v << ") -> " << retval << std::endl;
-    return retval;
-  }
-
-  virtual Vec3 random(const Vec3 &o) const {
-    auto random_point = Point3(random_double(x0, x1), k, random_double(z0, z1));
-    return random_point - o;
-  }
+  virtual Vec3 random(const Vec3 &o) const;
 
 public:
   double x0, x1, z0, z1, k;
   Material *material;
 };
 
-bool XzRect::hit(const Ray &r, double t_min, double t_max, HitRecord &rec) const
-{
-  auto t = (k-r.origin().y()) / r.direction().y();
-  if(t < t_min || t > t_max)
-    return false;
-
-  auto x = r.origin().x() + t*r.direction().x();
-  auto z = r.origin().z() + t*r.direction().z();
-  if(x < x0 || x > x1 || z < z0 || z > z1)
-    return false;
-
-  rec.u = (x - x0) / (x1 - x0);
-  rec.v = (z - z0) / (z1 - z0);
-  rec.t = t;
-
-  auto outward_normal = Vec3(0, 1, 0);
-  rec.set_face_normal(r, outward_normal);
-  rec.material = material;
-  rec.p = r.at(t);
-
-  return true;
-}
 
 class YzRect : public Hittable {
 public:
-  YzRect() {}
+  YzRect();
 
-  YzRect(
-    double y0, double y1, double z0, double z1, double k, Material *mat
-  )
-    : y0(y0), y1(y1), z0(z0), z1(z1), k(k), material(mat) {};
+  YzRect(double y0, double y1, double z0, double z1, double k, Material *mat);
 
   virtual bool hit(const Ray &r, double t_min, double t_max, HitRecord &rec) const override;
 
-  virtual bool bounding_box(double time0, double time1, Aabb &output_box) const override
-  {
-    // The bounding box must have non-zero width in each dimension, so pad the Z
-    // dimension a small amount.
-    output_box = Aabb(Point3(k-0.0001, y0, z0), Point3(k+0.0001, y1, z1));
-    return true;
-  }
+  virtual bool bounding_box(double time0, double time1, Aabb &output_box) const override;
 
 public:
   double y0, y1, z0, z1, k;
   Material *material;
 };
-
-bool YzRect::hit(const Ray &r, double t_min, double t_max, HitRecord &rec) const
-{
-  auto t = (k-r.origin().x()) / r.direction().x();
-  if(t < t_min || t > t_max)
-    return false;
-
-  auto y = r.origin().y() + t*r.direction().y();
-  auto z = r.origin().z() + t*r.direction().z();
-  if(y < y0 || y > y1 || z < z0 || z > z1)
-    return false;
-
-  rec.u = (y - y0) / (y1 - y0);
-  rec.v = (z - z0) / (z1 - z0);
-  rec.t = t;
-
-  auto outward_normal = Vec3(1, 0, 0);
-  rec.set_face_normal(r, outward_normal);
-  rec.material = material;
-  rec.p = r.at(t);
-
-  return true;
-}
 
 #endif
